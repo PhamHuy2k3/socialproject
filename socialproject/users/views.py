2f5e0035d78689af4a5848b5ff6489f7b7392f88
Pham Quoc Huy <itachihuy212@gmail.com>
2026-01-08 14:10:05 +0700

    New Update

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -5,5 +5,9 @@ from .form import LoginForm
 
 # Create your views here.
 def user_login(request):
-    form = LoginForm(request.POST)
-    return render(request, 'users/login.html', {'form': form})
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        return render(request, 'users/login.html', {'form': form})
+    else:
+        form = LoginForm()
+        return render(request, 'users/login.html', {'form': form})
