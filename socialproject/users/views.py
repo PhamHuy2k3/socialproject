7dd63c017dc8275af745ad37f4a0fda3edda5562
Pham Quoc Huy <itachihuy212@gmail.com>
2026-01-08 14:18:55 +0700

    update view

--- socialproject/users/views.py
+++ socialproject/users/views.py
@@ -1,5 +1,6 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 from .form import LoginForm
 
 
@@ -7,7 +8,16 @@ from .form import LoginForm
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        return render(request, 'users/login.html', {'form': form})
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password']
+            )
+            if user is not None:
+                login(request, user)
+                return HTTPResponse("Login Successful")
+            else:
+                return HTTPResponse("Invalid credentials")
     else:
         form = LoginForm()
         return render(request, 'users/login.html', {'form': form})
